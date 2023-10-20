0998b94bf787e336971451a064dbfc4187ffdbef
Alexander Kubyshev <akubyshev@gmail.com>
2023-10-20 20:27:18 +0300

    Homework Vue.js Seminar#6

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+import ProductDetails from './components/ProductDetails.vue';
+<template>
+  <MainPage />
+  <BlogPage />
+  <BlogDetailsPage />
+  <ProjectComp />
+</template>
+
+<script>
+import MainPage from "./components/MainPage.vue";
+import BlogPage from "./components/BlogPage.vue";
+import BlogDetailsPage from "./components/BlogDetailsPage.vue";
+import ProjectComp from "./components/ProjectComp.vue";
+
+export default {
+  components: {
+    MainPage,
+    BlogPage,
+    BlogDetailsPage,
+    ProjectComp,
+  },
+};
+</script>
+
+<style lang="scss"></style>
